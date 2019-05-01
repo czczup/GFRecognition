aba442c2f00fb02f952a9a5f3a48545f514593fd
nonoka <wztxy89@163.com>
2019-05-01 17:49:32 +0800

    my baseline
    仅使用图像数据，验证集准确率35%；
    进使用访问数据，验证集准确率55%。

---
+++ code/tfrecord_visit.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import os
+import random
+import sys
+import pandas as pd
+import numpy as np
+
+
+def get_data(dataset):
+    print("Loading dataset...")
+    table = pd.read_csv("../data/"+dataset, header=None)
+    filenames = [item[0].split("/")[-1].split('.')[0] for item in table.values]
+    class_ids = [int(item[0].split("/")[-1].split("_")[-1].split(".")[0])-1 for item in table.values]
+    data = []
+    for index, filename in enumerate(filenames):
+        visit = np.load("../data/train_visit/"+filename+".npy")
+        label = class_ids[index]
+        data.append([visit, label])
+    random.seed(0)
+    random.shuffle(data)
+    print("Loading completed...")
+    return data
+
+
+def int64_feature(values):
+    if not isinstance(values, (tuple, list)):
+        values = [values]
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=values))
+
+
+def bytes_feature(values):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[values]))
+
+
+def image_to_tfexample(data, label):
+    return tf.train.Example(features=tf.train.Features(feature={
+        'visit': bytes_feature(data),
+        'label': int64_feature(label),
+    }))
+
+
+def _convert_dataset(data, tfrecord_path, dataset):
+    """ Convert data to TFRecord format. """
+    output_filename = os.path.join(tfrecord_path, dataset+"_visit.tfrecord")
+    tfrecord_writer = tf.python_io.TFRecordWriter(output_filename)
+    length = len(data)
+    for index, item in enumerate(data):
+        data_ = item[0].tobytes()
+        label = item[1]
+        example = image_to_tfexample(data_, label)
+        tfrecord_writer.write(example.SerializeToString())
+        sys.stdout.write('\r>> Converting image %d/%d' % (index + 1, length))
+        sys.stdout.flush()
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    file = input("train or valid: ")
+    if file == "train":
+        data = get_data("train_oversampling.txt")
+        _convert_dataset(data, "../data/", "train")
+    elif file == "valid":
+        data = get_data("valid.txt")
+        _convert_dataset(data, "../data/", "valid")
